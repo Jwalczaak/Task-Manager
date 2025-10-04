1b1263361fae7a1ecd4af6fbcda766b85d89bfed
Jwalczaak <j.walczaak@gmail.com>
2025-10-04 11:30:59 +0200

    Added primeng and primeng table with hardcoded data

---
+++ TaskManager/src/app/app.config.ts
@@ -0,0 +1,26 @@
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    importProvidersFrom(HttpClientModule),
+  ],
+};
